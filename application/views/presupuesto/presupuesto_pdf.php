246d2dd3f4632b12986d9b0359f5ab2850bd1b87
Sky3dz <43693652+sky3dz@users.noreply.github.com>
2019-07-12 12:46:32 -0500

    reporte

--- application/views/presupuesto/presupuesto_pdf.php
+++ application/views/presupuesto/presupuesto_pdf.php
@@ -2,13 +2,30 @@
     /** @var object $pres */
 
     tcpdf();
-    $pdf = new TCPDF('P', PDF_UNIT, 'A6', true, 'UTF-8', false);
+    $pdf = new TCPDF('P', PDF_UNIT, 'A4', true, 'UTF-8', false);
+
     $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
-    $pdf->SetMargins(2, 2, 2);
+    //editado
+    //$pdf->SetMargins(2, 2, 2);
+
+    // set margins
+    $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+    $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+    $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+
     $pdf->setPrintHeader(false);
     $pdf->setPrintFooter(false);
-    $pdf->SetAutoPageBreak(false);
+
+    // set default header data
+    $pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 001', PDF_HEADER_STRING, array(0,64,255), array(0,64,128));
+    $pdf->setFooterData(array(0,64,0), array(0,64,128));
+
+    //editado
+    //$pdf->SetAutoPageBreak(false);
     $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+     // set auto page breaks
+    $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+
     $pdf->AddPage();
 
     $pdf->setFontStretching(92);
@@ -21,7 +38,20 @@
     $CENTER     = 'C';
     $lineHeight = 5.2;
 
-    $pdf->Cell(15, 0, "Fecha:", 0, false, $LEFT);
+    $pdf->SetCreator(PDF_CREATOR);
+
+    $pdf->SetAuthor('Nicola Asuni');
+
+    $pdf->SetTitle("Presupuesto CSM-$pres->pres_cod");
+
+    $pdf->SetFont('times', 'BI', 20);
+
+
+    $pdf->Cell(105, 5, "Fecha:$pres->pres_fecha_emision", 0, false, $RIGHT);
+
     $pdf->Cell(15, 0, "Presupuesto Nº: $pres->pres_cod", 0, false, $LEFT);
 
-    $pdf->Output("comprobante1.pdf", 'I');
+
+    $pdf->Output("comprobante$pres->pres_cod.pdf", 'I');
+
+
